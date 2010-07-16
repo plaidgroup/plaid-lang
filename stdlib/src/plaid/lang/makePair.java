bc0fa925c258cd98b586ee5c4e8795ea84570ae7
Mark Hahnenberg <mhahnenb@gmail.com>
2010-07-16 16:41:27 -0400

    Added a new AST node for Destructive Dereference, made some parser parser changes for that, and added very hacky initial version of unchecked exceptions.

--- stdlib/src/plaid/lang/makePair.java
+++ stdlib/src/plaid/lang/makePair.java
@@ -21,14 +21,14 @@
 package plaid.lang;
 @plaid.runtime.annotations.RepresentsMethod(name = "makePair", toplevel = true)
 public class makePair{
-	public static final java.util.List<plaid.runtime.utils.Import> vAr361$plaid;
+	public static final java.util.List<plaid.runtime.utils.Import> vAr397$plaid;
 	static {
-		vAr361$plaid = new java.util.ArrayList<plaid.runtime.utils.Import>();
-		plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr361$plaid",vAr361$plaid);
-		vAr361$plaid.add(new plaid.runtime.utils.Import("plaid.lang.*"));
-		vAr361$plaid.add(new plaid.runtime.utils.Import("plaid.lang.globals.*"));
+		vAr397$plaid = new java.util.ArrayList<plaid.runtime.utils.Import>();
+		plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr397$plaid",vAr397$plaid);
+		vAr397$plaid.add(new plaid.runtime.utils.Import("plaid.lang.*"));
+		vAr397$plaid.add(new plaid.runtime.utils.Import("plaid.lang.globals.*"));
 	}
-	public static final plaid.runtime.PlaidScope global$c0pe = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().globalScope("plaid.lang",vAr361$plaid);
+	public static final plaid.runtime.PlaidScope global$c0pe = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().globalScope("plaid.lang",vAr397$plaid);
 	@plaid.runtime.annotations.RepresentsMethod(name = "makePair", toplevel = false)
 	public static final plaid.runtime.PlaidMethod makePair_func;
 	static {
@@ -39,102 +39,93 @@ public class makePair{
 				final plaid.runtime.PlaidScope local$c0pe = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().localScope(temp$c0pe);
 				if (pA1R$plaid instanceof plaid.runtime.models.map.PlaidLookupMap) throw new plaid.runtime.PlaidException("No object found to insert into scope");
 				else local$c0pe.insert("pA1R$plaid", pA1R$plaid, false);
-				plaid.runtime.PlaidObject vAr360$plaid=null;
-				plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",34,17);
+				plaid.runtime.PlaidObject vAr396$plaid=null;
+				plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",34,19);
 				{
-					final plaid.runtime.PlaidObject f;
-					final plaid.runtime.PlaidObject vAr362$plaid;
-					final plaid.runtime.PlaidObject vAr363$plaid;
-					final plaid.runtime.PlaidObject vAr364$plaid;
-					vAr364$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("pA1R$plaid", local$c0pe);
-					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr364$plaid",vAr364$plaid);
-					vAr362$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("first", vAr364$plaid);
-					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr362$plaid",vAr362$plaid);
-					vAr363$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().unit();
-					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr363$plaid",vAr363$plaid);
-					f = plaid.runtime.Util.call(vAr362$plaid, vAr363$plaid);
-					plaid.runtime.PlaidRuntime.getRuntime().updateVar("f",f);
-					if (f instanceof plaid.runtime.models.map.PlaidLookupMap) throw new plaid.runtime.PlaidException("No object found to insert into scope");
-					else local$c0pe.insert("f", f, true);
-					plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",34,19);
+					final plaid.runtime.PlaidObject s;
+					final plaid.runtime.PlaidObject vAr398$plaid;
+					final plaid.runtime.PlaidObject vAr399$plaid;
+					final plaid.runtime.PlaidObject vAr400$plaid;
+					vAr400$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("pA1R$plaid", local$c0pe);
+					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr400$plaid",vAr400$plaid);
+					vAr398$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("first", vAr400$plaid);
+					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr398$plaid",vAr398$plaid);
+					vAr399$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().unit();
+					plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr399$plaid",vAr399$plaid);
+					s = plaid.runtime.Util.call(vAr398$plaid, vAr399$plaid);
+					plaid.runtime.PlaidRuntime.getRuntime().updateVar("s",s);
+					if (s instanceof plaid.runtime.models.map.PlaidLookupMap) throw new plaid.runtime.PlaidException("No object found to insert into scope");
+					else local$c0pe.insert("s", s, false);
+					plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",34,17);
 					{
-						final plaid.runtime.PlaidObject s;
-						final plaid.runtime.PlaidObject vAr365$plaid;
-						final plaid.runtime.PlaidObject vAr366$plaid;
-						final plaid.runtime.PlaidObject vAr367$plaid;
-						final plaid.runtime.PlaidObject vAr368$plaid;
-						final plaid.runtime.PlaidObject vAr369$plaid;
-						final plaid.runtime.PlaidObject vAr370$plaid;
-						vAr370$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("pA1R$plaid", local$c0pe);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr370$plaid",vAr370$plaid);
-						vAr368$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("second", vAr370$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr368$plaid",vAr368$plaid);
-						vAr369$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().unit();
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr369$plaid",vAr369$plaid);
-						vAr367$plaid = plaid.runtime.Util.call(vAr368$plaid, vAr369$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr367$plaid",vAr367$plaid);
-						vAr365$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("first", vAr367$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr365$plaid",vAr365$plaid);
-						vAr366$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().unit();
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr366$plaid",vAr366$plaid);
-						s = plaid.runtime.Util.call(vAr365$plaid, vAr366$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("s",s);
-						if (s instanceof plaid.runtime.models.map.PlaidLookupMap) throw new plaid.runtime.PlaidException("No object found to insert into scope");
-						else local$c0pe.insert("s", s, true);
-						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,9);
-						final plaid.runtime.PlaidState vAr371$plaid;
-						final plaid.runtime.PlaidObject vAr372$plaid;
-						vAr372$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("Pair", global$c0pe);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr372$plaid",vAr372$plaid);
-						final plaid.runtime.PlaidState vAr374$plaid;
-						vAr374$plaid = plaid.runtime.Util.toPlaidState(vAr372$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr374$plaid",vAr374$plaid);
-						final plaid.runtime.PlaidState vAr373$plaid;
-						vAr373$plaid = plaid.runtime.Util.newState();
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr373$plaid",vAr373$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,20);
+						final plaid.runtime.PlaidObject f;
+						final plaid.runtime.PlaidObject vAr401$plaid;
+						final plaid.runtime.PlaidObject vAr402$plaid;
+						final plaid.runtime.PlaidObject vAr403$plaid;
+						vAr403$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("pA1R$plaid", local$c0pe);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr403$plaid",vAr403$plaid);
+						vAr401$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("second", vAr403$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr401$plaid",vAr401$plaid);
+						vAr402$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().unit();
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr402$plaid",vAr402$plaid);
+						f = plaid.runtime.Util.call(vAr401$plaid, vAr402$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("f",f);
+						if (f instanceof plaid.runtime.models.map.PlaidLookupMap) throw new plaid.runtime.PlaidException("No object found to insert into scope");
+						else local$c0pe.insert("f", f, false);
+						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,9);
+						final plaid.runtime.PlaidState vAr404$plaid;
+						final plaid.runtime.PlaidObject vAr405$plaid;
+						vAr405$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("Pair", global$c0pe);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr405$plaid",vAr405$plaid);
+						final plaid.runtime.PlaidState vAr407$plaid;
+						vAr407$plaid = plaid.runtime.Util.toPlaidState(vAr405$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr407$plaid",vAr407$plaid);
+						final plaid.runtime.PlaidState vAr406$plaid;
+						vAr406$plaid = plaid.runtime.Util.newState();
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr406$plaid",vAr406$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,20);
 						@plaid.runtime.annotations.RepresentsField(name = "fst", toplevel = false)
-						final plaid.runtime.PlaidObject vAr376$plaid;
-						vAr376$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().protoField(new plaid.runtime.utils.Delegate () {
-							public plaid.runtime.PlaidObject invoke(final plaid.runtime.PlaidObject this$plaid, final plaid.runtime.PlaidObject vAr377$plaid) {
+						final plaid.runtime.PlaidObject vAr409$plaid;
+						vAr409$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().protoField(new plaid.runtime.utils.Delegate () {
+							public plaid.runtime.PlaidObject invoke(final plaid.runtime.PlaidObject this$plaid, final plaid.runtime.PlaidObject vAr410$plaid) {
 								final plaid.runtime.PlaidScope temp$c0pe = local$c0pe;
 								final plaid.runtime.PlaidScope local$c0pe = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().localScope(temp$c0pe);
-								final plaid.runtime.PlaidObject vAr375$plaid;
-								plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,26);
-								vAr375$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("f", local$c0pe);
-								plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr375$plaid",vAr375$plaid);
-								return vAr375$plaid;
+								final plaid.runtime.PlaidObject vAr408$plaid;
+								plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,26);
+								vAr408$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("f", local$c0pe);
+								plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr408$plaid",vAr408$plaid);
+								return vAr408$plaid;
 							}
 						}
 						);
-						final plaid.runtime.PlaidMemberDef vAr378$plaid;
-						vAr378$plaid = plaid.runtime.Util.anonymousMemberDef("fst", false, false);
-						vAr373$plaid.addMember(vAr378$plaid,vAr376$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,29);
+						final plaid.runtime.PlaidMemberDef vAr411$plaid;
+						vAr411$plaid = plaid.runtime.Util.anonymousMemberDef("fst", false, false);
+						vAr406$plaid.addMember(vAr411$plaid,vAr409$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,29);
 						@plaid.runtime.annotations.RepresentsField(name = "snd", toplevel = false)
-						final plaid.runtime.PlaidObject vAr380$plaid;
-						vAr380$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().protoField(new plaid.runtime.utils.Delegate () {
-							public plaid.runtime.PlaidObject invoke(final plaid.runtime.PlaidObject this$plaid, final plaid.runtime.PlaidObject vAr381$plaid) {
+						final plaid.runtime.PlaidObject vAr413$plaid;
+						vAr413$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().protoField(new plaid.runtime.utils.Delegate () {
+							public plaid.runtime.PlaidObject invoke(final plaid.runtime.PlaidObject this$plaid, final plaid.runtime.PlaidObject vAr414$plaid) {
 								final plaid.runtime.PlaidScope temp$c0pe = local$c0pe;
 								final plaid.runtime.PlaidScope local$c0pe = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().localScope(temp$c0pe);
-								final plaid.runtime.PlaidObject vAr379$plaid;
-								plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/kbn/Documents/workspace/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,35);
-								vAr379$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("s", local$c0pe);
-								plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr379$plaid",vAr379$plaid);
-								return vAr379$plaid;
+								final plaid.runtime.PlaidObject vAr412$plaid;
+								plaid.runtime.PlaidRuntime.getRuntime().updateLocation("/Users/mhahnenberg/Desktop/plaid-lang/stdlib/src/plaid/lang/Pair.plaid",35,35);
+								vAr412$plaid = plaid.runtime.PlaidRuntime.getRuntime().getClassLoader().lookup("s", local$c0pe);
+								plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr412$plaid",vAr412$plaid);
+								return vAr412$plaid;
 							}
 						}
 						);
-						final plaid.runtime.PlaidMemberDef vAr382$plaid;
-						vAr382$plaid = plaid.runtime.Util.anonymousMemberDef("snd", false, false);
-						vAr373$plaid.addMember(vAr382$plaid,vAr380$plaid);
-						vAr371$plaid = vAr374$plaid.initState(vAr373$plaid);
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr371$plaid",vAr371$plaid);
-						vAr360$plaid = vAr371$plaid.instantiate();
-						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr360$plaid",vAr360$plaid);
+						final plaid.runtime.PlaidMemberDef vAr415$plaid;
+						vAr415$plaid = plaid.runtime.Util.anonymousMemberDef("snd", false, false);
+						vAr406$plaid.addMember(vAr415$plaid,vAr413$plaid);
+						vAr404$plaid = vAr407$plaid.initState(vAr406$plaid);
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr404$plaid",vAr404$plaid);
+						vAr396$plaid = vAr404$plaid.instantiate();
+						plaid.runtime.PlaidRuntime.getRuntime().updateVar("vAr396$plaid",vAr396$plaid);
 					}
 				}
-				return vAr360$plaid;
+				return vAr396$plaid;
 			}
 		}
 		);
