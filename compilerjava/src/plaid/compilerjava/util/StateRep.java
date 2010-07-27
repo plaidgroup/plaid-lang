ef10992d312442b9669d0df14728990f803b2ee6
Mark Hahnenberg <mhahnenb@gmail.com>
2010-07-27 13:44:10 -0400

    Post-merge commit.

---
+++ compilerjava/src/plaid/compilerjava/util/StateRep.java
@@ -0,0 +1,66 @@
+package plaid.compilerjava.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StateRep extends MemberRep {
+	private List<String> declaredMembers = new ArrayList<String>();
+	private List<String> needs = new ArrayList<String>();
+	
+	public StateRep(String name) {
+		super(name);
+	}
+	
+	public void addMember(String memberName) {
+		declaredMembers.add(memberName);
+	}
+	
+	public void addMembers(List<String> members) {
+		declaredMembers.addAll(members);
+	}
+	public List<String> getMembers() {
+		return Collections.unmodifiableList(declaredMembers);
+	}
+	
+	public void addNeed(String path) {
+		needs.add(path);
+	}
+	
+	public void addNeeds(List<String> newNeeds) {
+		needs.addAll(newNeeds);
+	}
+	
+	public void setNeeds(List<String> newNeeds) {
+		needs = newNeeds;
+	}
+	
+	public void removeNeed(String path) {
+		needs.remove(path);
+	}
+	
+	public List<String> getNeeds() {
+		return Collections.unmodifiableList(needs);
+	}
+	
+	public boolean hasNeeds() {
+		return !needs.isEmpty();
+	}
+	
+	public String toString() {
+		StringBuilder toRet  = new StringBuilder("state " + getName() + "(");
+		for (String s : declaredMembers) toRet.append(s + ",");
+		if (toRet.lastIndexOf(",") > -1)
+			toRet.setCharAt(toRet.lastIndexOf(","), ')');
+		else
+			toRet.append(")");
+		if (!needs.isEmpty()) {
+			toRet.append(" Needs(");
+			for (String n : needs) toRet.append(n + ",");
+			toRet.setCharAt(toRet.lastIndexOf(","), ')');
+		}
+		return toRet.toString();
+
+	}
+	
+}
---
+++ stdlib/src/build/PlaidStandardLibrary.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2010 The Plaid Group (see AUTHORS file)
+ * 
+ * This file is part of Plaid Programming Language.
+ *
+ * Plaid Programming Language is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  Plaid Programming Language is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Plaid Programming Language.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package build;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+import plaid.compilerjava.CompilerConfiguration;
+import plaid.compilerjava.CompilerCore;
+
+public class PlaidStandardLibrary {
+//	private File f;
+//	public PlaidStandardLibrary(File f) {
+//		this.f = f;
+//	}
+	
+	@Test
+	public void BuildPlaidStandardLibrary () throws FileNotFoundException {
+		CompilerConfiguration cc = new CompilerConfiguration();
+		cc.setOutputDir("src");
+		for (File f : inputFiles())
+			cc.addInputFile(f);
+		cc.setKeepTemporaryFiles(true);
+		cc.setInvokeCompiler(false);
+		cc.setDebugMode(true);
+		cc.setCopyrightHeader(true);
+		cc.addToPlaidPath("bin/");
+		CompilerCore compiler = new CompilerCore(cc);
+		compiler.compile();
+	}
+
+	private static List<File> inputFiles() {
+		List<File> results = new ArrayList<File>();
+	    String currentdir = System.getProperty("user.dir") + System.getProperty("file.separator"); // should be work space by default
+	    File cur = new File(currentdir);
+	    assertTrue( cur.isDirectory() );
+	    for (File f : cur.listFiles()) {
+			if ( f.getName().startsWith("plaid")) {
+				List<File> files = new ArrayList<File>();
+				findPlaidFile(f, files);
+				for (File file : files) {
+					File f1 = new File(file.getAbsolutePath().substring(currentdir.length()));
+					results.add(f1);
+				}
+			}
+		}
+	    return results;
+	}
+	
+	private static void findPlaidFile(File f, List<File> files) {
+		if ( f.isFile() ) {
+			if ( f.getName().endsWith(".plaid")) {
+				files.add(f);
+			}
+		} else if ( f.isDirectory() ) {		
+			for (File file : f.listFiles()) {
+				findPlaidFile(file, files);
+			}
+		}
+	}
+	
+}
