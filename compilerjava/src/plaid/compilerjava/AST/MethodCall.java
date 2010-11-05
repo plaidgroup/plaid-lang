48d06c198652161cf80247e0e6615a21c6f66157
Manuel Mohr <manuelmohr@gmail.com>
2010-11-05 18:10:08 -0400

    Temporary fix of code generation for MethodCall nodes.

--- compilerjava/src/plaid/compilerjava/AST/MethodCall.java
+++ compilerjava/src/plaid/compilerjava/AST/MethodCall.java
@@ -8,31 +8,32 @@ import plaid.compilerjava.coreparser.Token;
 import plaid.compilerjava.tools.ASTVisitor;
 import plaid.compilerjava.util.CodeGen;
 import plaid.compilerjava.util.IDList;
+import plaid.compilerjava.util.IdGen;
 
 public class MethodCall implements Expression {
 
 	private final Token callSite;
-	private final List<Expression> arguments;
+	private final Expression argument;
 	private final Expression receiver;
 	private final ID method;
 	
-	public MethodCall(Token callSite, Expression receiver, ID method, List<Expression> arguments) {
+	public MethodCall(Token callSite, Expression receiver, ID method, Expression argument) {
 		this.callSite = callSite;
-		this.arguments = arguments;
+		this.argument = argument;
 		this.receiver = receiver;
 		this.method = method;
 	}
 	
-	public MethodCall(Token callSite, Expression receiver, ID method, Expression argument) {
-		this.callSite = callSite;
-		this.arguments = new ArrayList<Expression>();
-		this.arguments.add(argument);
-		this.receiver = receiver;
-		this.method = method;
-	}
+//	public MethodCall(Token callSite, Expression receiver, ID method, Expression argument) {
+//		this.callSite = callSite;
+//		this.arguments = new ArrayList<Expression>();
+//		this.arguments.add(argument);
+//		this.receiver = receiver;
+//		this.method = method;
+//	}
 
-	public List<Expression> getArguments() {
-		return arguments;
+	public Expression getArgument() {
+		return argument;
 	}
 
 	public Expression getReceiver() {
@@ -44,10 +45,28 @@ public class MethodCall implements Expression {
 	}
 
 	@Override
-	public void codegenExpr(CodeGen out, ID y, IDList localVars,
-			Set<ID> stateVars) {
-		// TODO Auto-generated method stub
-
+	public void codegenExpr(CodeGen out, ID y, IDList localVars, Set<ID> stateVars) {
+		out.setLocation(callSite);
+		
+		// Evaluate the receiver.
+		ID o = IdGen.getId();
+		out.declareFinalVar(CodeGen.plaidObjectType, o.getName());
+		receiver.codegenExpr(out, o, localVars, stateVars);
+		
+		// Lookup the method.
+		ID m = IdGen.getId();
+		out.declareFinalVar(CodeGen.plaidObjectType, m.getName());
+		out.assignToLookup(m.getName(), CodeGen.convertOpNames(method.getName()), o.getName());
+		
+		// Evaluate the argument.
+		ID a = IdGen.getId();
+		out.declareFinalVar(CodeGen.plaidObjectType, a.getName());
+		argument.codegenExpr(out, a, localVars, stateVars);
+		
+		// Call the method.
+		ID p = IdGen.getId();
+		out.declareFinalVar(CodeGen.plaidObjectType, p.getName());
+		out.assignToCall(y.getName(), m.getName(), a.getName());
 	}
 
 	@Override
@@ -62,15 +81,13 @@ public class MethodCall implements Expression {
 
 	@Override
 	public boolean hasToken() {
-		return true;
+		return callSite != null;
 	}
 
 	@Override
 	public <T> void visitChildren(ASTVisitor<T> visitor) {
 		receiver.accept(visitor);
 		method.accept(visitor);
-		for (Expression e : arguments) e.accept(visitor);
-
+		argument.accept(visitor);
 	}
-
 }
