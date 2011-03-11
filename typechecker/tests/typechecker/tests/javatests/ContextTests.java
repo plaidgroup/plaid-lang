8272c80f783205a266dd8329ea6e70429ab53186
kbn <kbn@localhost>
2011-03-11 16:20:51 -0500

    more binding method and tests

---
+++ typechecker/tests/typechecker/tests/javatests/ContextTests.java
@@ -0,0 +1,25 @@
+package typechecker.tests.javatests;
+
+import org.junit.Test;
+
+public class ContextTests {
+	@Test
+	public void testPutAndGet() {
+		// TODO: stub
+	}
+	
+	@Test
+	public void testDemote() {
+		// TODO: stub
+	}
+	
+	@Test
+	public void testCopy() {
+		// TODO: stub
+	}
+	
+	@Test
+	public void testRemove() {
+		// TODO: stub
+	}
+}
