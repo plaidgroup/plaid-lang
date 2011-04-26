c359d00d596b57ffaf12bf8556fbdece1e60fc9b
sven.stork.CMU <sven.stork.CMU@localhost>
2011-04-26 23:01:25 +0100

    - add Method MetaParameter translation

--- compilerjava/src/plaid/compilerjava/types/TypeParameterType.java
+++ compilerjava/src/plaid/compilerjava/types/TypeParameterType.java
@@ -4,19 +4,19 @@ import plaid.compilerjava.AST.ID;
 
 public class TypeParameterType implements MetaType, Type {
 	private final ID id;
-	private final Type caseOf;
+	private final ObjectType caseOf;
 	
-	public TypeParameterType(ID id, Type caseOf) {
+	public TypeParameterType(ID id, ObjectType caseOf) {
 		super();
 		this.id = id;
 		this.caseOf = caseOf;
 	}
 
-	public ID getId() {
+	public ID getID() {
 		return id;
 	}
 
-	public Type getCaseOf() {
+	public ObjectType getCaseOf() {
 		return caseOf;
 	}
 }
