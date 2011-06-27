b65c9909cd681840463a0a4acaf5bba2f45c808e
Sarah Chasins <schasins@gmail.com>
2011-06-27 14:49:40 -0400

    js runtime including member and tag tests

--- runtimejs/tests/jasmine-standalone-1.1.0.rc1/spec/PlaidObjectSpec.js
+++ runtimejs/tests/jasmine-standalone-1.1.0.rc1/spec/PlaidObjectSpec.js
@@ -1,14 +1,8 @@
 describe("PlaidObject", function() {
-  var metadata1=[["",[],"with"],[["car",[],"with"],[["drivingStatus",[],"with"],[["driving",["speed","acceleration","stopDriving"],""],[["brakingStatus",[],"with"],[["braking",["stopBraking"],""]]],[["directionStatus",[],"with"],[["turningLeft",["straight","turnRight"],""]]]]],[["cleanStatus",[],"with"],[["clean",["getDirty"],""]]]]];
-
-  var metadata2=[["",[],"with"],[["brakingStatus",[],"with"],[["notBraking",["startBraking"],""],[["speedIncreasing",[],""]]]]];
   var obj1;
   var obj2;
-  var obj3;
-  var treeBuilder;
-  var md3;
 
-  treeBuilder=new Tree();
+  var treeBuilder=new Tree();
   var car = new Node("car",[],"with",treeBuilder.root);
   var drivingStatus = new Node("drivingStatus",[],"with",car);
   var driving = new Node("driving",["speed","acceleration","stopDriving"],"case of",drivingStatus);
@@ -18,20 +12,17 @@ describe("PlaidObject", function() {
   var turningLeft = new Node("turningLeft",["straight","turnRight"], "case of", directionStatus);
   var cleanStatus = new Node("cleanStatus",[],"with",car);
   var clean = new Node("clean",["getDirty"],"case of",cleanStatus);
-  md3 = treeBuilder.toMetadata();
+  var md3 = treeBuilder.toMetadata();
 
 
   beforeEach(function() {
-    obj1 = new PlaidObject(metadata1);
-    obj2 = new PlaidObject(metadata2);
+    obj1 = new PlaidObject(md3);
     
   });
 
   it("should be able to match a tag in it", function() {
-    jasmine.log(metadata1);
-    jasmine.log(md3);
     var result=obj1.match("turningLeft");
-    expect(result).toBe(false);
+    expect(result).toBe(true);
   });
 
   it("should fail to match a tag not in it", function() {
@@ -43,6 +34,12 @@ describe("PlaidObject", function() {
     var result=obj1.tags();
     var desiredTags=[ 'car', 'drivingStatus', 'driving', 'brakingStatus', 'braking', 'directionStatus', 'turningLeft', 'cleanStatus', 'clean' ];
     expect(result).toHaveSameElementsAs(desiredTags);
+  });
+
+  it("should have expected member list", function() {
+    var result=obj1.members();
+    var desiredMembers=[ "speed","acceleration","stopDriving","stopBraking","straight","turnRight","getDirty"];
+    expect(result).toHaveSameElementsAs(desiredMembers);
   });
 
   
