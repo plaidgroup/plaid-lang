9fed7c75630be84a782c830752470eef3d9629ed
Sarah Chasins <schasins@gmail.com>
2011-06-28 17:20:46 -0400

    added more tests for js runtime, corrected errors in runtime itself

--- runtimejs/src/PlaidState.js
+++ runtimejs/src/PlaidState.js
@@ -81,7 +81,8 @@ PlaidState.prototype.instantiate = function() {
 
 /*returns a copy of the state on which it was called, copy lacks the member that was passed in*/
 PlaidState.prototype.remove = function(member) {
-   var obj=this.instantiate();
+   var obj=new PlaidState(this.tree.clone());
+   copyMembers(obj,this);
    if(s_remove(obj.tree,member)===false){
       throw "Error: attempt to remove a member "+member+" that does not exist in the state.<br>";
       return;
@@ -96,7 +97,8 @@ PlaidState.prototype.rename = function(member, newName) {
    if (has(currMembers,newName)){
       throw "Error: attempt to rename a member with name "+newName+" which already names a member.<br>";
    }
-   var obj=this.instantiate();
+   var obj=new PlaidState(this.tree.clone());
+   copyMembers(obj,this);
    if(s_rename(obj.tree,member,newName)===false){
       throw "Error: attempt to rename a member "+member+" that does not exist in the state.<br>";
       return;
@@ -106,25 +108,73 @@ PlaidState.prototype.rename = function(member, newName) {
    return obj;
 }
 
+/*returns an array of all the tags of the state that is reflected in the current metadata*/
+function s_tags(md1){
+   var tagList=[];
+   if (md1[0][0]!==""){
+      tagList=[md1[0][0]];
+   }
+   var length=md1.length;
+   for (var i=1;i<length;i++){
+      tagList=tagList.concat(s_tags(md1[i]));
+   }
+   return tagList;
+}
+
 /*returns a new state composed of both the state on which the function was called and the state that was passed in*/
 PlaidState.prototype.with = function(state) {
-   var obj=new PlaidObject(this.tree.clone());
+
+   var md1=this.tree;
+   var md2=state.tree;
+
+   //check unique tags
+   var tags1=s_tags(md1);
+   var tags2=s_tags(md2);
+   var tags1Length=tags1.length;
+   for (var j=0;j<tags1Length;j++){
+      if(has(tags2,tags1[j])){
+         throw "Error: with operation violates unique tags by containing tag "+tags[j]+" in both component states.<br>";
+      }
+   } 
+
+   //check unique members
+   var members1=s_members(md1);
+   var members2=s_members(md2);
+   var members1Length=members1.length;
+   for (var j=0;j<members1Length;j++){
+      if(has(members2,members1[j])){
+         throw "Error: with operation violates unique members by containing member "+tags[j]+" in both component states.<br>";
+      }
+   } 
+
+   var obj=new PlaidState(this.tree.clone());
    var i;
    copyMembers(obj,this);
    copyMembers(obj,state);
-   var md2=state.tree;
    var md2Length=md2.length;
       for (var i=1;i<md2Length;i++){
-         obj.tree.push(md2.clone());
-      }
+         obj.tree.push(md2[i].clone());
+      }  
+
    return obj;
+}
+
+/*Returns true if item is contained in array, false if it is not*/
+function has(array, item){
+   var length=array.length;
+   for (var i=0;i<length;i++){
+      if (array[i]===item){
+         return true;
+      }
+   }
+   return false;
 }
 
 /*copies members of obj2 to obj1*/
 function copyMembers(obj1,obj2){
    var i;
    for (i in obj2) {
-      if (!obj2.hasOwnProperty(i)||i==="tree") {
+      if (i==="tree" || i==="instantiate" || i==="members" || i==="remove" || i==="rename" || i==="with" || i==="clone") {
          continue;
       }
       if (obj2[i] && typeof obj2[i] == "object") {
