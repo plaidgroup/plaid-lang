688019b45b2965f793b41f6f7f87e29d58d9d7c1
jsunshine <jsunshine@localhost>
2011-07-26 17:05:58 -0400

    Fixed bugs in equivalent test. Added whole very simple whole program parser test.

--- parser/src/plaid/parser/test/ParserTests.java
+++ parser/src/plaid/parser/test/ParserTests.java
@@ -5,15 +5,17 @@ package plaid.parser.test;
 
 
 import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import plaid.parser.ParseException;
 import plaid.parser.ParserCore;
+import plaid.parser.ast.*;
+import plaid.parser.ast.Specifier.SpecifierKind;
 
 /**
  * @author jssunshi
@@ -25,5 +27,24 @@ public class ParserTests {
 	public void noPackage() throws ParseException {
 		String program = "val foo = 5;";
 		ParserCore.parse(new StringBufferInputStream(program));
+	}
+	
+	@Test
+	public void intField() throws ParseException {
+		String program = "package plaid;\n val foo = 5;";
+		CompilationUnit parsedCU = ParserCore.parse(new StringBufferInputStream(program));
+		Decl field = 
+			new ConcreteFieldDecl(null, new ArrayList<Modifier>(), 
+					new Specifier(null,SpecifierKind.VAL),
+					null,
+					new Identifier(null,"foo"), 
+					new IntLiteral(null,5));
+		QualifiedIdentifier packageName = 
+			new QualifiedIdentifier(null,Collections.singletonList(new Identifier(null, "plaid")));
+		CompilationUnit goalCU = new CompilationUnit(Collections.singletonList(field),
+				new ArrayList<Import>(), packageName);
+		Assert.assertNotNull("Parsed CU is null", parsedCU);
+		boolean match = goalCU.equivalent(parsedCU);
+		Assert.assertTrue("Parsed and goal CUs are not equivalent.", match);
 	}
 }
