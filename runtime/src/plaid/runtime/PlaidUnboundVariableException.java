c04dff266adb365019ebaad0e906193a3d030340
karl.naden <karl.naden@localhost>
2011-07-26 01:03:24 -0400

    typechecker script method

---
+++ runtime/src/plaid/runtime/PlaidUnboundVariableException.java
@@ -0,0 +1,10 @@
+package plaid.runtime;
+
+public class PlaidUnboundVariableException extends PlaidRuntimeException {
+	public PlaidUnboundVariableException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
