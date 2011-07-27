db35027a6b38958d4799432964aa4f8e963ee404
sven <sven@localhost>
2011-07-27 11:20:39 -0400

    - fix parser bug that confuses smaller with meta arguments

---
+++ parser/src/plaid/parser/ast/FieldDecl.java
@@ -0,0 +1,23 @@
+package plaid.parser.ast;
+
+import java.util.List;
+
+import plaid.parser.Token;
+
+public abstract class FieldDecl extends Decl {
+
+	private final Specifier specifier;
+	private final Type type; 
+	public FieldDecl(Token t, List<Modifier> modifiers,
+			Specifier specifier, Type type, Identifier name) {
+		super(t, modifiers, name);
+		this.specifier = specifier;
+		this.type = type;
+	}
+	public Specifier getSpecifier() {
+		return specifier;
+	}
+	public Type getType() {
+		return type;
+	}
+}
