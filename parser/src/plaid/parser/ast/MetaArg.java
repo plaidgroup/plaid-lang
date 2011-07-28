36e9c0ce9011f9f558098fba045dc4c2461d7582
jsunshine <jsunshine@localhost>
2011-07-28 13:49:09 -0400

    Updated generated AST

---
+++ parser/src/plaid/parser/ast/MetaArg.java
@@ -0,0 +1,11 @@
+package plaid.parser.ast;
+
+import plaid.parser.Token;
+
+public class MetaArg extends ASTNode {
+
+	public MetaArg(Token token) {
+		super(token);
+	}
+
+}
---
+++ parser/src/plaid/parser/ast/MetaGroupArgSpec.java
@@ -0,0 +1,31 @@
+package plaid.parser.ast;
+
+import plaid.parser.Token;
+
+public class MetaGroupArgSpec extends MetaArgSpec {
+
+	private final GroupPermission gp;
+	private final Identifier id;
+
+	public MetaGroupArgSpec(Token token, GroupPermission gp, Identifier id) {
+		super(token);
+		this.gp = gp;
+		this.id = id;
+	}
+
+	public GroupPermission getGp() {
+		return gp;
+	}
+
+	public Identifier getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("group ");
+		sb.append(id.toString());
+		return sb.toString();
+	}
+}
