275245928d4cee19555af5261c488a064d329d4e
sven <sven@localhost>
2011-08-05 15:56:11 -0400

    - add this to context

---
+++ parser/src/plaid/parser/ast/StaticType.java
@@ -0,0 +1,11 @@
+package plaid.parser.ast;
+
+import plaid.parser.Token;
+
+public class StaticType extends Type {
+
+	public StaticType(Token token) {
+		super(token);
+	}
+
+}
---
+++ stdlib/src/plaid/lang/_native.java
@@ -0,0 +1,9 @@
+package plaid.lang;
+
+public final class _native {
+	private _native() {}
+	
+	public static boolean refEq(Object o1, Object o2) {
+		return o1 == o2;
+	}
+}
