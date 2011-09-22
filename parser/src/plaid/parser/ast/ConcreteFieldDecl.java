9bbb79b22df01eeff6c117b8c53a7b0b128f10f6
karl.naden <karl.naden@localhost>
2011-09-22 17:48:26 -0400

    remove main function breaking imports

---
+++ parser/src/plaid/parser/ast/ConcreteFieldDecl.java
@@ -0,0 +1,34 @@
+package plaid.parser.ast;
+
+import java.util.List;
+
+import plaid.parser.Token;
+
+public class ConcreteFieldDecl extends FieldDecl {
+	private final Expr expression;
+
+	public ConcreteFieldDecl(Token t, List<Modifier> modifiers, 
+			Specifier specifier, Type fieldType,
+			Identifier name,  Expr expr) {
+		super(t, modifiers, specifier, fieldType, name);
+		this.expression = expr;
+	}
+
+	public Expr getExpression() {
+		return expression;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		if (getSpecifier()!= Specifier.EMPTY ) sb.append(getSpecifier().toString() + " ");
+		if (getFieldType() != Type.EMPTY ) sb.append(getFieldType().toString()+ " ");
+		sb.append(getName());
+		sb.append("=");
+		sb.append(expression.toString());
+		sb.append(";");
+		
+		return sb.toString();
+	}
+}
