93da3e618441b78a20701703ed3fa2f1955c6fd1
jsunshine <jsunshine@localhost>
2011-10-19 15:51:41 -0400

    Working game of life in Plaid

---
+++ demo/src/plaid/demo/Coordinate.java
@@ -0,0 +1,17 @@
+package plaid.demo;
+
+public class Coordinate {
+	private final int column;
+	private final int row;
+	public Coordinate(int column, int row) {
+		super();
+		this.column = column;
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public int getRow() {
+		return row;
+	}
+}
