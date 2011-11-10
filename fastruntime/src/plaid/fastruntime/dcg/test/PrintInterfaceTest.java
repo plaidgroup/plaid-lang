833fed641d067f5453bb718932cf0c95d22cde80
jsunshine <jsunshine@localhost>
2011-11-10 17:22:36 -0500

    updated storage test

--- fastruntime/src/plaid/fastruntime/dcg/test/PrintInterfaceTest.java
+++
@@ -1,9 +0,0 @@
-package plaid.fastruntime.dcg.test;
-
-import plaid.fastruntime.dcg.InterfaceGenerator;
-
-public class PrintInterfaceTest {
-	public static void main(String[] args) {
-		new InterfaceGenerator().createInterface("bar", 1);
-	}
-}
