41cdf93b0af8d1a2824cb598912eb3e7c965b369
Sven Stork <Sven Stork@localhost>
2012-01-02 08:54:23 -0500

    reformating

--- parser/src/plaid/parser/ast/ConcreteStateDecl.java
+++ parser/src/plaid/parser/ast/ConcreteStateDecl.java
@@ -7,9 +7,13 @@ import plaid.parser.Token;
 public final class ConcreteStateDecl extends StateDecl {
 	private final StateExpr stateBinding;
 
-	public ConcreteStateDecl(Token t, List<Annotation> annotations, List<Modifier> modifiers,
-			Identifier name, List<StaticArg> StaticArgsSpec,
-			QualifiedIdentifier caseOf, List<Expr> metaCaseOfArgs,
+	public ConcreteStateDecl(Token t, 
+			List<Annotation> annotations, 
+			List<Modifier> modifiers,
+			Identifier name, 
+			List<StaticArg> StaticArgsSpec,
+			QualifiedIdentifier caseOf, 
+			List<Expr> metaCaseOfArgs,
 			StateExpr stateBinding) {
 		super(t, annotations, modifiers, name, StaticArgsSpec, caseOf, metaCaseOfArgs);
 		this.stateBinding = stateBinding;
