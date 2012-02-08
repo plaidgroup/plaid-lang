13059468e3179f2b1af0772a3f0f6f3926633c33
Sven Stork <devel@svenstork.com>
2012-02-07 19:59:18 -0500

    import required symbols

---
+++ faststdlib/src/plaid/lang/String$plaid.java
@@ -0,0 +1,27 @@
+package plaid.lang;
+
+import plaid.fastruntime.PlaidObject;
+import plaid.fastruntime.PlaidState;
+import plaid.fastruntime.PlaidStorage;
+
+public class String$plaid implements PlaidObject{
+
+	@Override
+	public PlaidState getDispatch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PlaidStorage getStorage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void changeState(PlaidState s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ parser/src/plaid/parser/ast/AtomicBlock.java
@@ -0,0 +1,32 @@
+package plaid.parser.ast;
+
+import java.util.List;
+import java.util.Map;
+
+import plaid.parser.Token;
+
+public class AtomicBlock extends Expr {
+        private final List<Expr> datagroups;
+        private final Expr body;
+
+		public AtomicBlock(Token token, List<Expr> datagroups, Expr body) {
+                super(token);
+                this.datagroups = datagroups;
+                this.body = body;
+        }
+
+        public List<Expr> getDatagroups() {
+			return datagroups;
+		}
+
+		public Expr getBody() {
+			return body;
+		}
+
+		@Override
+		public String toString() {
+			return "atomic<"+exprListToString(datagroups)+">" + body.toString();			
+		}
+}
+        
+        
