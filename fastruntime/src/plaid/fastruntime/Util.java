07a3f75d523569dec706202868afb7a3ba66dbc4
karl.naden <karl.naden@gmail.com>
2012-02-12 23:46:07 -0500

    updated names to prevent strange interactions with the old runtime

--- fastruntime/src/plaid/fastruntime/Util.java
+++ fastruntime/src/plaid/fastruntime/Util.java
@@ -4,9 +4,9 @@ import plaid.fastruntime.dcg.DispatchGenerator;
 import plaid.fastruntime.dcg.InterfaceGenerator;
 import plaid.fastruntime.dcg.JavaDispatchGenerator;
 import plaid.fastruntime.dcg.StorageGenerator;
-import plaid.lang.Boolean$plaid;
+import plaid.lang.Bool$plaid;
 import plaid.lang.DPrintStream$plaid;
-import plaid.lang.Integer$plaid;
+import plaid.lang.Pint$plaid;
 import plaid.lang.String$plaid;
 
 public class Util {
@@ -15,7 +15,7 @@ public class Util {
 	static {
 		JAVA_GEN.preloadPlaidState(java.io.PrintStream.class, new DPrintStream$plaid());
 		JAVA_GEN.preloadPlaidState(java.lang.String.class, new String$plaid());
-		JAVA_GEN.preloadPlaidState(java.lang.Integer.class, new Integer$plaid());
+		JAVA_GEN.preloadPlaidState(java.lang.Integer.class, new Pint$plaid());
 //		JAVA_GEN.preloadPlaidState(plaid.lang.Boolean$plaid.True.class, new Boolean$plaid(null));
 //		JAVA_GEN.preloadPlaidState(plaid.lang.Boolean$plaid.False.class, new Boolean$plaid(null));
 	}
