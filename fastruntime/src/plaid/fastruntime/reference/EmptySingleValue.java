22a1b98257d9a73349c04f565c31c545493619e3
Josh Sunshine <jsunshine@gmail.com>
2012-03-08 16:38:30 -0500

    EmptySingleValue

---
+++ fastruntime/src/plaid/fastruntime/reference/EmptySingleValue.java
@@ -0,0 +1,42 @@
+package plaid.fastruntime.reference;
+
+import plaid.fastruntime.FieldInfo;
+import plaid.fastruntime.MethodInfo;
+import plaid.fastruntime.ObjectValue;
+import fj.Ord;
+import fj.data.List;
+import fj.data.Set;
+
+public class EmptySingleValue extends SingleValue {
+
+	@Override
+	public List<MethodInfo> getMethods() {
+		return List.nil();
+	}
+
+	@Override
+	public List<FieldInfo> getFields() {
+		return List.nil();
+	}
+
+	@Override
+	public ObjectValue remove(String member) {
+		return this;
+	}
+
+	@Override
+	public Set<String> getTags() {
+		return Set.empty(Ord.stringOrd);
+	}
+
+	@Override
+	public Set<String> getOuterTags() {
+		return Set.empty(Ord.stringOrd);
+	}
+
+	@Override
+	public Set<String> getInnerTags() {
+		return Set.empty(Ord.stringOrd);
+	}
+
+}
