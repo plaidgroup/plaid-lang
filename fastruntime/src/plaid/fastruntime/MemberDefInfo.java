da0b28f5357ff803c94dec51a4010eed18d68aa3
Josh Sunshine <jsunshine@gmail.com>
2012-03-21 08:56:10 -0400

    Fixed canonical rep for dynamically defined fields. Adjusted test accordingly.

---
+++ fastruntime/src/plaid/fastruntime/MemberDefInfo.java
@@ -0,0 +1,10 @@
+package plaid.fastruntime;
+
+
+public interface MemberDefInfo extends Comparable<MemberDefInfo>{
+	/**
+	 * @return String in UUID form.
+	 */
+	public String getMemberDefId();
+	public Object getMemberDefInstance();
+}
