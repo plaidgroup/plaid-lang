807f591f2fd146d610a880832d4ddd47ed5bbd4d
karl.naden <karl.naden@gmail.com>
2012-03-27 15:38:15 -0400

    update error reporting to include a static error Code and a dynamic data message specific to the error instance

--- fastruntime/src/plaid/fastruntime/ErrorCodes.java
+++ fastruntime/src/plaid/fastruntime/ErrorCodes.java
@@ -14,7 +14,22 @@ public class ErrorCodes {
 		ERROR_MESSAGES.put("T_ASSIGN_LOCAL_VAL",
 						   "Cannot assign to a \"val\" variable or parameter");
 		
+		
+		/*
+		 *  PERMISSION ERROR MESSAGES
+		 */
+		ERROR_MESSAGES.put("P_RETURN_FAIL",
+				   "(Implementation Error) Invalid Permission Return: ");
 	
+	}
+	
+	public static final String getMessage(String errorCode) {
+		if (ERROR_MESSAGES.containsKey(errorCode)) {
+			return ERROR_MESSAGES.get(errorCode);
+		 } else {
+			return "Unknown Error Code: " + errorCode;
+		 }
+		
 	}
 	
 }
