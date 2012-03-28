2fcc0caf6a5c25bbcd417040322ca401cce65142
Sven Stork <devel@svenstork.com>
2012-03-27 21:55:46 -0400

    code cleanup + finals for Floats

--- fastruntime/src/plaid/fastruntime/aeminium/Util.java
+++ fastruntime/src/plaid/fastruntime/aeminium/Util.java
@@ -11,16 +11,9 @@ public final class Util {
 	public static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
 	
 	public static final boolean parallelize() {
-		//return POOL.getActiveThreadCount() < CPU_COUNT;
-		//return POOL.getQueuedTaskCount() < 1;
-		//return POOL.getRunningThreadCount() < CPU_COUNT;
-		//return POOL.getQueuedSubmissionCount() < 2;
 		ForkJoinWorkerThread wthread = (ForkJoinWorkerThread)Thread.currentThread();
 		return     wthread.getLocalQueueSize() < 1 
 				&& POOL.getActiveThreadCount() < CPU_COUNT;
-//		return     POOL.getActiveThreadCount() < CPU_COUNT
-//				&& POOL.getQueuedTaskCount() < CPU_COUNT/4
-//				&& POOL.getQueuedSubmissionCount() < CPU_COUNT/4;
 	}
 	
 }
