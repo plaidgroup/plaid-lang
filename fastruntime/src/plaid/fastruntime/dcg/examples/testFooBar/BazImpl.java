d26cced80e2486b2a607f7fbd3bb5dc0e1d934cf
jsunshine <jsunshine@localhost>
2012-03-29 11:56:06 -0400

    Consolidated all new compiler junit tests into tests project, added new source directory in tests project labeled "junit" to contain these tests, moved some tests out of old source direcotry "src"

--- fastruntime/src/plaid/fastruntime/dcg/examples/testFooBar/BazImpl.java
+++
@@ -1,9 +0,0 @@
-package plaid.fastruntime.dcg.examples.testFooBar;
-
-import plaid.fastruntime.dcg.MethodImplementation;
-
-public class BazImpl implements MethodImplementation {
-	public static void baz(Object thi$) {
-		System.out.println("baz from " + thi$);
-	}
-}
