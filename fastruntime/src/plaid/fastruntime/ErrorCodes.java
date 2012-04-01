17611972b6cf97a760093689e018cfa2b31f7597
karl.naden <karl.naden@gmail.com>
2012-04-01 00:14:04 -0400

    revamp of typechecker errors and some new features (typechecking specializations)

--- fastruntime/src/plaid/fastruntime/ErrorCodes.java
+++ fastruntime/src/plaid/fastruntime/ErrorCodes.java
@@ -8,18 +8,151 @@ public class ErrorCodes {
 	public static final Map<String,String> ERROR_MESSAGES = new HashMap<String,String>();
 	static {
 		
+		/* 
+		 *  INTERNAL ERROR MESSAGES
+		 */
+		ERROR_MESSAGES.put("SYM_NOT_FOUND", 
+			"(Implementation Error) Symbol not found.");
+		ERROR_MESSAGES.put("UNEXPECTED_SYM",
+			"(Implementation Error) unexpected symbol found: ");
+		ERROR_MESSAGES.put("STRUCT_NOT_FOUND", 
+			"(Implementation Error) Structure not found");
+		ERROR_MESSAGES.put("SCOPING_ERROR", 
+			"(Implementation Error) Expected symbol already out of scope: ");
+		ERROR_MESSAGES.put("T_WRONG_AST",
+			"(Implementation Error) Bad AST structure: ");
+		ERROR_MESSAGES.put("UNEXPECTED_AST",
+			"(Implementation Error) Unexpected AST node: ");
+		ERROR_MESSAGES.put("UNIMPLEMENTED_FEATURE",
+			"(Unimplemented Feature) ");
+		
 		/*
 		 *  TYPECHECKER ERROR MESSAGES
 		 */
+		ERROR_MESSAGES.put("T_NOT_IN_SCOPE",
+		   "Identifier not in scope: ");
 		ERROR_MESSAGES.put("T_ASSIGN_LOCAL_VAL",
-						   "Cannot assign to a \"val\" variable or parameter");
+			"Cannot assign to a 'val' variable or parameter: ");
+		ERROR_MESSAGES.put("T_ASSIGN_WRONG_TYPE",
+		   "Wrong type provided for assignment: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_METHOD",
+		   "Cannot assign to a method: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_OTHER",
+		   "(Implementation Error) unknown member signature: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_MISSING",
+		   "Field does not exist: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_NON_OBJECT_STRUCTURE",
+			"Target object of assigment is not an object: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_NOT_ENOUGH_PERMISSION",
+			"Insufficient permission for field assignment: ");
+		ERROR_MESSAGES.put("T_ASSIGN_FIELD_WRONG_TARGET",
+			"Target of field assignment must be an identifier");
+		ERROR_MESSAGES.put("T_ASSIGN_PERM",
+			"Assignment have permission none: ");
+		ERROR_MESSAGES.put("T_DEREF_METHOD",
+		   "Cannot dereference a method: ");
+		ERROR_MESSAGES.put("T_DEREF_MISSING",
+		   "Field does not exist: ");
+		ERROR_MESSAGES.put("T_DOUBLE_PERM",
+		   "Double literals are immutable: ");
+		ERROR_MESSAGES.put("T_INT_PERM",
+		   "Integer literals are immutable: ");
+		ERROR_MESSAGES.put("T_STRING_PERM",
+		   "String literals are immutable: ");
+		ERROR_MESSAGES.put("CHAIN_DEREF",
+		   "Cannot chain dereferences");
+		ERROR_MESSAGES.put("T_CASE_NOT_SUBSTRUCT", 
+		   "Case pattern must be a substructure of matched structure: ");
+		ERROR_MESSAGES.put("T_CASE_NOT_STATE", 
+		   "Case pattern must be a state");
+		ERROR_MESSAGES.put("T_MATCH_NO_CASES", 
+		   "No cases found for match");
+		ERROR_MESSAGES.put("MATCH_NON_ID",
+		   "Match target expression must be an ID");
+		ERROR_MESSAGES.put("T_CONTEXT_MERGE_FAIL",
+		   "Could not merge contexts");
+		ERROR_MESSAGES.put("T_NO_RESULT_LUB",
+		   "No consistent result type");
+		ERROR_MESSAGES.put("T_CALL_NOT_METHOD",
+		   "Cannot call a non-method: ");
+		ERROR_MESSAGES.put("T_CALL_MISSING",
+		   "Method not found: ");
+		ERROR_MESSAGES.put("T_CALL_ARG_NUM",
+		   "Wrong number of arguments provided: ");
+		ERROR_MESSAGES.put("T_CALL_WRONG_ARG_STRUCT",
+		   "Incompatible argument structure provided: ");
+		ERROR_MESSAGES.put("T_CALL_NON_OBJECT_STRUCTURE",
+			"Target object of method call is not an object: ");
+		ERROR_MESSAGES.put("RESTORE_VAR", 
+			"Cannot restore to a 'var': ");	
+		ERROR_MESSAGES.put("T_VAR_ALREADY_DECLARED",
+			"Local variable name already in scope: ");	
+		ERROR_MESSAGES.put("T_VAR_WRONG_STRUCT",
+			"Local variable initializer gives wrong structure: ");	
+		ERROR_MESSAGES.put("NESTED_STATES", 
+			"Nested states not supported.");	
+		ERROR_MESSAGES.put("STATE_COMPOSITION", 
+			"State composition not supported.");	
+		ERROR_MESSAGES.put("STATE_REF_WRONG_AST", 
+			"States rep not supported: ");
+		ERROR_MESSAGES.put("T_NEW_NON_STATE", 
+			"Target of 'new' not a state: ");
+		ERROR_MESSAGES.put("T_SPEC_ABSTRACT", 
+			"Cannot specialize with an abstract member.");
+		ERROR_MESSAGES.put("SPECIALIZATIONS", 
+			"Specialize if and only if abstract.");
+		ERROR_MESSAGES.put("T_NEW_ABSTRACT_MEMBER", 
+			"No definition given for abstract member: ");
+		ERROR_MESSAGES.put("T_NEW_BAD_SPEC", 
+			"Invalid implementation of abstract member: ");
+		ERROR_MESSAGES.put("T_NEW_SPEC_WRONG_SYM", 
+			"Unexpected symbol for specialization: ");
+		ERROR_MESSAGES.put("T_FIELD_DECL_STRUCT", 
+			"Field Initializer structure is not a substructure of declared structure: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_RCVR_STRUCT_IN", 
+			"Receiver structure not guaranteed by declaring state: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_MULTIPLE_INPUTS", 
+			"Multiple inputs defined with the same name: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_ENV_CHANGE", 
+			"Method environment variables cannot consume permissions or change out of starting state: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_ENV_SCOPE", 
+			"Method environment variable not in scope at declaration: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_ENV_STRUCT", 
+			"Method environment variable not in scope at declaration: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_RET_STRUCT", 
+			"Method body do not provide required return structure: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_RCVR_TYPE_OUT", 
+			"Method receiver did not end with the right type: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_ARG_TYPE_OUT", 
+			"Arg did not end with the right type: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_ENV_TYPE_OUT", 
+			"Environment variable did not end with the right type: ");
+		ERROR_MESSAGES.put("T_CHNG_NOT_CHANGEABLE", 
+			"Cannot change state of object with permission ");
+		ERROR_MESSAGES.put("T_CHNG_PERM",
+			"State changes have permission none: ");
+		ERROR_MESSAGES.put("T_METHOD_DECL_TOP_RCVR",
+			"Receiver of top level method must be void: ");
+	
 		
+		/*
+		 * Aeminium Error messages
+		 */
+		
+		ERROR_MESSAGES.put("A_ASSIGN_SHARED_OBJECT_FIELD",
+		   "Assignment to fields of shared objects require protection of an atomic block.");
+		ERROR_MESSAGES.put("A_DEPENDENCY_MISSING",
+		   "Cannot find dependency information: ");
+		ERROR_MESSAGES.put("A_NESTED_ATOMIC",
+		   "Nested Atomic blocks found");
 		
 		/*
 		 *  PERMISSION ERROR MESSAGES
 		 */
 		ERROR_MESSAGES.put("P_RETURN_FAIL",
 				   "(Implementation Error) Invalid Permission Return: ");
+		ERROR_MESSAGES.put("P_SPLIT_FAIL",
+				"Could not split permission: ");
 		
 		/*
 		 *  STRUCTURE ERROR MESSAGES
@@ -37,7 +170,9 @@ public class ErrorCodes {
 		ERROR_MESSAGES.put("S_ASSIGN_OTHER",
 				"(Implementation Error) Cannot assign to non-field member: ");	
 		ERROR_MESSAGES.put("S_ASSIGN_MISSING",
-				"(Implementation Error) Cannot assign to missing member: ");	
+				"(Implementation Error) Cannot assign to missing member: ");
+		ERROR_MESSAGES.put("S_MERGE_FAILED",
+				"Structures cannot be merged: ");
 	
 	}
 	
