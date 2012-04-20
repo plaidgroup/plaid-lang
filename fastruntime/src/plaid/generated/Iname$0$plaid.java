a65b6ef510d204861c0945d33c466e056425a757
Sven Stork <devel@svenstork.com>
2012-04-20 10:29:38 -0400

    name resolve static arg expressions

---
+++ fastruntime/src/plaid/generated/Iname$0$plaid.java
@@ -0,0 +1,9 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface Iname$0$plaid {
+
+	PlaidObject name(PlaidObject vaR4$plaid);
+
+}
---
+++ fastruntime/src/plaid/generated/Ipipepipe$plaid$1$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface Ipipepipe$plaid$1$plaid {
+	PlaidObject pipepipe$plaid(PlaidObject receiver, PlaidObject arg);
+}
