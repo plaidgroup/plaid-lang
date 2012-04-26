7189e2b476c4aaf47f30d6bd012ce82a6c498679
Josh Sunshine <jsunshine@gmail.com>
2012-04-25 20:35:59 -0400

    Rename TestClass to conform to naming convention in tests directory

--- tests/junit/plaid/tests/runtime/ExampleClass.java
+++ tests/junit/plaid/tests/runtime/ExampleClass.java
@@ -1,6 +1,6 @@
 package plaid.tests.runtime;
 
-public class TestClass {
+public class ExampleClass {
 	public static int addTwo(int x, int y) { 
 		return x + y; 
 	}
@@ -11,28 +11,28 @@ public class TestClass {
 	
 	public final String s; 
 	
-	public TestClass() {
+	public ExampleClass() {
 		s = "noArgs";
 	}
 	
-	public TestClass(String s) {
+	public ExampleClass(String s) {
 		this.s = s;
 	}
 	
-	public TestClass(Object o1, Object o2) {
+	public ExampleClass(Object o1, Object o2) {
 		this.s = "object object";
 	}
 	
-	public TestClass(Object o1, String s) {
+	public ExampleClass(Object o1, String s) {
 		this.s = "object " + s;
 	}
 	
-	public TestClass(String s, Object o) {
+	public ExampleClass(String s, Object o) {
 		this.s = s + " object";
 	}
 	
-	public TestClass appendFoo(Object o) {
-		return new TestClass(this.s + " objectFoo");
+	public ExampleClass appendFoo(Object o) {
+		return new ExampleClass(this.s + " objectFoo");
 	}
 	
 	public static void main(String[] args) {
