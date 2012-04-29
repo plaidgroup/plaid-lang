7ec4f86cb4acb67766419a87d3ef2566c6b86ca1
Josh Sunshine <jsunshine@gmail.com>
2012-04-29 15:33:33 -0400

    updates specialize tests to conform to new runtime methodology

--- tests/junit/plaid/tests/runtime/SpecializeTest.java
+++ tests/junit/plaid/tests/runtime/SpecializeTest.java
@@ -1,72 +1,76 @@
 package plaid.tests.runtime;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
-import junit.framework.Assert;
 import plaid.fastruntime.ObjectValue;
-import plaid.fastruntime.reference.ObjectValueFactory;
+import plaid.fastruntime.Util;
 
 public class SpecializeTest {
 
 	@Test
 	public void specializeParentA() {
-		ObjectValueFactory ovf = new ObjectValueFactory();
-		ovf.tag("BAR");
-		ovf.mthd("A", 0, "test");
-		ovf.mthd("B", 0, "test");
-		ObjectValue parent = ovf.value();
-		ovf = new ObjectValueFactory();
-		ovf.tag("FOO");
-		ovf.parent(parent);
-		ObjectValue firstValue = ovf.value();
+		ObjectValue ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addMethod("A", 0, "test");
+		ovf = ovf.addMethod("B", 0, "test");
+		ovf = ovf.addTag("BAR");
+		ObjectValue parent = ovf;
 		
-		ovf = new ObjectValueFactory();
-		ovf.mthd("A", 0, "test");
-		ObjectValue specializer = ovf.value();
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("FOO");
+		ovf = ovf.addParent(parent);
+		ObjectValue firstValue = ovf;
 		
-		ObjectValue actualValue = firstValue.specialize(specializer);
+		ovf = firstValue;
+		ovf = ovf.remove("A");
+		ovf = ovf.addMethod("A", 0, "test");
 		
-		ovf = new ObjectValueFactory();
-		ovf.tag("BAR");
-		ovf.mthd("B", 0, "test");
-		ObjectValue parent2 = ovf.value();
-		ovf = new ObjectValueFactory();
-		ovf.tag("FOO");
-		ovf.parent(parent2);
-		ovf.mthd("A", 0, "test");
-		ObjectValue expectedValue = ovf.value();
+		ObjectValue actualValue = ovf;
+		
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("BAR");
+		ovf = ovf.addMethod("B", 0, "test");
+		ObjectValue parent2 = ovf;
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("FOO");
+		ovf = ovf.addParent(parent2);
+		ovf = ovf.addMethod("A", 0, "test");
+		ObjectValue expectedValue = ovf;
 		
 		Assert.assertEquals(expectedValue, actualValue);
 	}
 	
 	@Test
 	public void specializeParentAB() {
-		ObjectValueFactory ovf = new ObjectValueFactory();
-		ovf.tag("BAR");
-		ovf.mthd("A", 0, "test");
-		ovf.mthd("B", 0, "test");
-		ObjectValue parent = ovf.value();
-		ovf = new ObjectValueFactory();
-		ovf.tag("FOO");
-		ovf.parent(parent);
-		ObjectValue firstValue = ovf.value();
+		ObjectValue ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("BAR");
+		ovf = ovf.addMethod("A", 0, "test");
+		ovf = ovf.addMethod("B", 0, "test");
+		ObjectValue parent = ovf;
+
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("FOO");
+		ovf = ovf.addParent(parent);
+		ObjectValue firstValue = ovf;
 		
-		ovf = new ObjectValueFactory();
-		ovf.mthd("A", 0, "max");
-		ovf.mthd("B", 0, "max2");
-		ObjectValue specializer = ovf.value();
+		ovf = firstValue;
+		ovf = ovf.remove("A");
+		ovf = ovf.remove("B");
+		ovf = ovf.addMethod("A", 0, "max");
+		ovf = ovf.addMethod("B", 0, "max2");
 		
-		ObjectValue actualValue = firstValue.specialize(specializer);
+		ObjectValue actualValue = ovf;
 		
-		ovf = new ObjectValueFactory();
-		ovf.tag("BAR");
-		ObjectValue parent2 = ovf.value();
-		ovf = new ObjectValueFactory();
-		ovf.tag("FOO");
-		ovf.parent(parent2);
-		ovf.mthd("A", 0, "max");
-		ovf.mthd("B", 0, "max2");
-		ObjectValue expectedValue = ovf.value();
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("BAR");
+		ObjectValue parent2 = ovf;
+		ovf = Util.getEmptyObjectValue();
+		ovf = ovf.addTag("FOO");
+		ovf = ovf.addParent(parent2);
+		ovf = ovf.addMethod("A", 0, "max");
+		ovf = ovf.addMethod("B", 0, "max2");
+		ObjectValue expectedValue = ovf;
 		
 		Assert.assertEquals(expectedValue, actualValue);
 	}
