e0d3d9f93bedf1e358b0df6ca07a6ceb0013b614
karl.naden <karl.naden@gmail.com>
2012-05-02 23:41:56 -0400

    add hand-generated interface for test

---
+++ tests/junit/plaid/generated/IdoA_ddF_ive$1$plaid.java
@@ -0,0 +1,9 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface IdoA_ddF_ive$1$plaid {
+
+	public PlaidObject doA_ddF_ive(PlaidObject recvr, PlaidObject arg);
+	
+}
