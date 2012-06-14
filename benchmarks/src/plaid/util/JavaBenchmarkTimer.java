e49c86cf63899ef77000fb2f706844fd29bd23dd
Sven Stork <devel@svenstork.com>
2012-06-13 20:51:23 -0400

    add benchmark timer wrapper

--- benchmarks/src/plaid/util/JavaBenchmarkTimer.java
+++ benchmarks/src/plaid/util/JavaBenchmarkTimer.java
@@ -1,8 +1,17 @@
 package plaid.util;
 
+import java.text.DecimalFormat;
+
 public final class JavaBenchmarkTimer {
+	private static final DecimalFormat formatter = new DecimalFormat("#.###");
 	private long start;
 	private long lastMark;
+	
+	public JavaBenchmarkTimer() {}
+	
+	public static final JavaBenchmarkTimer make() {
+		return new JavaBenchmarkTimer();
+	}
 	
 	public void start() {
 		reset();
@@ -16,13 +25,14 @@ public final class JavaBenchmarkTimer {
 	public void mark(String msg) {
 		final long mark = System.nanoTime();
 		final double lastLap = (mark - this.lastMark) / (1000*1000*1000.0);
-		System.out.println(msg + " : " + lastLap + "s");
+		System.out.println(msg + " : " + formatter.format(lastLap) + "s");
+		this.lastMark = System.nanoTime();
 	}
 	
 	public void end(String msg) {
-		final long mark = System.nanoTime();
-		final double lastLap = (mark - this.start) / (1000*1000*1000.0);
-		System.out.println(msg + " : " + lastLap + "s");
+		mark(msg);
+		final double lastLap = (this.lastMark - this.start) / (1000*1000*1000.0);
+		System.out.println("total time : " + formatter.format(lastLap) + "s");
 		reset();
 	}
 }
