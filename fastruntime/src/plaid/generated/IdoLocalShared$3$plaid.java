d232cdd511887fa9a1fc0e97016b1833db1847a6
Sven Stork <devel@svenstork.com>
2012-06-22 19:02:12 -0400

    allow parameter passing to ops

---
+++ fastruntime/src/plaid/generated/IdoLocalShared$3$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface IdoLocalShared$3$plaid {
+	public PlaidObject doLocalShared(PlaidObject thisVar, PlaidObject index, PlaidObject ops, PlaidObject data);
+}
