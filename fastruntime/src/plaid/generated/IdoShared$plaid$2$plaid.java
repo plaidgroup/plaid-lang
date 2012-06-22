4fc8d249c9d0367e8c98cd830cb2e8e1ea02048d
Sven Stork <devel@svenstork.com>
2012-06-22 15:28:16 -0400

    add 'do' functions

---
+++ fastruntime/src/plaid/generated/IdoShared$plaid$2$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface IdoShared$plaid$2$plaid {
+	public PlaidObject doShared(PlaidObject thisVar, PlaidObject index, PlaidObject ops);
+}
