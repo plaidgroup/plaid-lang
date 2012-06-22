8001813018fb5ec0f9d0e8b9c5784775aed59b62
Sven Stork <devel@svenstork.com>
2012-06-22 14:28:41 -0400

    start working in Array Library

---
+++ fastruntime/src/plaid/generated/IsetUnique$plaid$2$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface IsetUnique$plaid$2$plaid {
+	public PlaidObject setUnique(PlaidObject thisVar, PlaidObject index, PlaidObject obj);
+}
