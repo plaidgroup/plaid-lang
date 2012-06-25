217066c7505d82a6e6b90a4774627a69222bdd6e
Sven Stork <devel@svenstork.com>
2012-06-25 11:19:13 -0400

    start working on global shared array implementation

---
+++ fastruntime/src/plaid/generated/IdoExclusive$1$plaid.java
@@ -0,0 +1,5 @@
+package plaid.generated;
+
+public interface IdoExclusive$1$plaid {
+	public abstract plaid.fastruntime.PlaidObject doExclusive(plaid.fastruntime.PlaidObject thisVar, plaid.fastruntime.PlaidObject obj);
+}
---
+++ fastruntime/src/plaid/generated/IdoSharedData2$3$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface IdoSharedData2$3$plaid {
+	public PlaidObject doSharedData2(PlaidObject thisVar, PlaidObject p1, PlaidObject p2, PlaidObject p3);
+}
