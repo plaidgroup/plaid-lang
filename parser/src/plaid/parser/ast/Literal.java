9fb71975f3305edeca1ec94e0ac2e1099271e5a0
Ben Chung <Ben Chung@BenChung-XPS.WV.CC.CMU.EDU>
2012-07-10 15:59:06 -0400

    Backed out changeset: 589ba644c6ea

--- parser/src/plaid/parser/ast/Literal.java
+++ parser/src/plaid/parser/ast/Literal.java
@@ -2,7 +2,7 @@ package plaid.parser.ast;
 
 import plaid.parser.Token;
 
-public class Literal extends Expr implements XMLEmbeddable {
+public class Literal extends Expr {
 
 	public Literal(Token token) {
 		super(token);
