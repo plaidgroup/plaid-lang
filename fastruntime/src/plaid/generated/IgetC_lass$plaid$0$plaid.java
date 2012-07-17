b1b822b173412f60e07643e197498ee159fcc343
Ben Chung <Ben Chung@BenChung-XPS.WV.CC.CMU.EDU>
2012-07-17 10:00:16 -0400

    Generated Int64

---
+++ fastruntime/src/plaid/generated/IgetC_lass$plaid$0$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+public interface IgetC_lass$plaid$0$plaid{
+
+	PlaidObject getC_lass$plaid(PlaidObject receiver);
+}
---
+++ fastruntime/src/plaid/generated/Iwait$plaid$0$plaid.java
@@ -0,0 +1,7 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+public interface Iwait$plaid$0$plaid{
+
+	PlaidObject wait$plaid(PlaidObject receiver);
+}
