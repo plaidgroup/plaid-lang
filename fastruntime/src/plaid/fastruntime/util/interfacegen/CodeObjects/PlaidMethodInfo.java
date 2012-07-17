ab6d48ae0dc2a370ad28bc295cfd1769f21a0332
Ben Chung <Ben Chung@BenChung-XPS.WV.CC.CMU.EDU>
2012-07-17 09:57:54 -0400

    Additional code generator fixes

--- fastruntime/src/plaid/fastruntime/util/interfacegen/CodeObjects/PlaidMethodInfo.java
+++ fastruntime/src/plaid/fastruntime/util/interfacegen/CodeObjects/PlaidMethodInfo.java
@@ -3,4 +3,6 @@ package plaid.fastruntime.util.interfacegen.CodeObjects;
 public interface PlaidMethodInfo {
 	public String getName();
 	public int getNArgs();
+	public Class[] getArgumentTypes();
+	public String getOperation();
 }
