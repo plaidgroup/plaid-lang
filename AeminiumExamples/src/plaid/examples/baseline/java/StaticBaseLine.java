d5a9989fa7f67dbfe613114c5eb0ff7062022ce4
Sven <Sven@localhost>
2012-07-19 15:25:29 -0400

    static base line

---
+++ AeminiumExamples/src/plaid/examples/baseline/java/StaticBaseLine.java
@@ -0,0 +1,18 @@
+package plaid.examples.baseline.java;
+
+public class StaticBaseLine {
+		
+	public static void forkJoin(int level) {
+		boolean isZero = level == 0;
+		if ( isZero == false ) {
+			int nextLevel = level - 1;
+			forkJoin(nextLevel);
+			forkJoin(nextLevel);
+		}
+	}
+
+	public static void main(String[] args) {
+		forkJoin(28);
+	}
+
+}
