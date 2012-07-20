5b67f2e11409c3c8c792b6f6e1d0cfc95cad04f6
Sven Stork <devel@svenstork.com>
2012-07-19 22:54:23 -0400

    fix benchmark

--- AeminiumExamples/src/plaid/examples/baseline/js/ObjectBaseLine.js
+++ AeminiumExamples/src/plaid/examples/baseline/js/ObjectBaseLine.js
@@ -1,15 +1,17 @@
-var BaseLine = {};
+function BaseLine() { 
 
-BaseLine.forkJoin = function (level) {
-    var isZero = level - 1;
+};        
+
+BaseLine.prototype.forkJoin = function (level) {
+    var isZero = level == 0;
     if  ( isZero == false ) {
         var nextLevel = level - 1;
         this.forkJoin(nextLevel);
         this.forkJoin(nextLevel);
     } 
 };
-    
 
-BaseLine.forkJoin(28);
+var bl = new BaseLine();
+bl.forkJoin(28);
 
 
