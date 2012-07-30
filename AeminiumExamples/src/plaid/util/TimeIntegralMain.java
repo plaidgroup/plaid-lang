6775d5c85391d30ac33344c66cac49822369713b
Sven Stork <devel@svenstork.com>
2012-07-30 16:14:31 -0400

    remove output to automize benchmark runner

--- AeminiumExamples/src/plaid/util/TimeIntegralMain.java
+++ AeminiumExamples/src/plaid/util/TimeIntegralMain.java
@@ -7,6 +7,6 @@ public class TimeIntegralMain {
 		//plaid.examples.integral.java.Integral.main(args);
 		final long duration = System.nanoTime() -startTime;
 		final double durSeconds = duration / 1000000000.0;
-		System.out.println("Ran for : " + durSeconds  + "s");
+		System.out.println("" + durSeconds  + "");
 	}
 }
