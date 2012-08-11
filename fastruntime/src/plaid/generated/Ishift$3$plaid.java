81b931c5453342ab75a0c8ec700dda9e62ea6fcb
Ben Chung <Ben Chung@BenChung-XPS>
2012-08-11 04:05:49 -0400

    Added interface to support new array operations

---
+++ fastruntime/src/plaid/generated/Ishift$3$plaid.java
@@ -0,0 +1,8 @@
+package plaid.generated;
+
+import plaid.fastruntime.PlaidObject;
+
+public interface Ishift$3$plaid {
+
+	PlaidObject shift(PlaidObject receiver, PlaidObject arg1, PlaidObject arg2, PlaidObject arg3);
+}
